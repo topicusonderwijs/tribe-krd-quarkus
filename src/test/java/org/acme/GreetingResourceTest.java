cb5eb01c3c9ec11e5bd56d75867671e962d45061
Martijn Dashorst <martijn.dashorst@topicus.nl>
2025-08-22 18:13:21 +0200

    Fix: testcase so the build succeeds

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -15,11 +15,12 @@
  */
 package org.acme;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -30,7 +31,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(is("Hello Tribe KRD!"));
     }
 
 }
